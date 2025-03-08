9caaa34ab45189ac7f25531456e4fcfbcb274662
Vadims <vadimsilatovskis@inbox.lv>
2025-03-08 14:10:00 +0200

    Lekcijas beigas
    Lekcijas beigas

---
+++ UI_RPG_Projekts/Assets/Sripts/Character.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+public int health = 5;
+
+public void ReportHealth()
+{
+    Debug.Log(name + "Health" + health);
+}
+
+}
